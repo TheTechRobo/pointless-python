3d2f7c0e2e89511f2d5a9e3df42e4f1d1b86891b
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-03-22 13:30:22 -0400

    Update thisisthemostpointlessgameyoulleverplay.py
    i'm an idiot

--- thisisthemostpointlessgameyoulleverplay.py
+++ thisisthemostpointlessgameyoulleverplay.py
@@ -1,6 +1,6 @@
 from random import randint as r
 n1 = r(0,12489) #random number from 0 to 12489
-n1 = r(0,12489)
+n2 = r(0,12489)
 question = "Guess my number. It's between ", n1, " and ", n2, "."
 
 try:
