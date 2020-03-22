068f21840d846024462bb031f250d3932649a375
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-03-22 13:29:58 -0400

    Update thisisthemostpointlessgameyoulleverplay.py
    this is starting to get embarrassing...

--- thisisthemostpointlessgameyoulleverplay.py
+++ thisisthemostpointlessgameyoulleverplay.py
@@ -1,7 +1,7 @@
 from random import randint as r
-question = "Guess my number. It's between ", n1, " and ", n2, "."
 n1 = r(0,12489) #random number from 0 to 12489
 n1 = r(0,12489)
+question = "Guess my number. It's between ", n1, " and ", n2, "."
 
 try:
     while True: #loop endlessly
