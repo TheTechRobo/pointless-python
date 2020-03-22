9e4da2470c33a4c3f9c912c6ef899ebdc5ce34ad
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-03-22 13:28:30 -0400

    Update thisisthemostpointlessgameyoulleverplay.py
    bugfix

--- thisisthemostpointlessgameyoulleverplay.py
+++ thisisthemostpointlessgameyoulleverplay.py
@@ -1,11 +1,11 @@
 from random import randint as r
-
+question = "Guess my number. It's between ", n1, " and ", n2, "."
 try:
     while True: #loop endlessly
         n1 = r(0,12489) #pick random number from 0 to 12489
         n2 = r(0,12489)
         try:
-            int(input("Guess my number. It's between ", n1, " and ", n2, ".")) #ask for a number
+            int(input(question)) #ask for a number
         except ValueError: #if they enter text instead of a number
             print("I asked for a number!")
         print("Nope, that's not right.\nGuess again!")
