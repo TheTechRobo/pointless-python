ebf765f051b6431be37ce8c5e0a2aa8b8e2f9ae8
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-03-22 13:24:26 -0400

    Create thisisthemostpointlessgameyoulleverplay.py
    thisisthemostpointlessgameyou'lleverplay

---
+++ thisisthemostpointlessgameyoulleverplay.py
@@ -0,0 +1,13 @@
+from random import randinit as r
+
+try:
+    while True: #loop endlessly
+        n1 = r(0,12489) #pick random number from 0 to 12489
+        n2 = r(0,12489)
+        try:
+            int(input("Guess my number. It's between ", n1, " and ", n2, ".")) #ask for a number
+        except ValueError: #if they enter text instead of a number
+            print("I asked for a number!")
+        print("Nope, that's not right.\nGuess again!")
+except KeyboardInterrupt: #if someone does Ctrl-C
+    print(":(")
